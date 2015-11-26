297c331c9a10d1ee2b491bc79d5ac34f1716aed2
Tomas Dvorak <todvora@gmail.com>
2015-11-26 17:29:32 +0100

    jshint fixes, plugin test disabled

--- spec/tester_spec.js
+++ spec/tester_spec.js
@@ -16,17 +16,17 @@ describe(__filename, function() {
     .done();
   });
 
-  it('should create book with plugins and parse content', function(testDone) {
-      tester.builder()
-      .withContent('This text is {% em %}highlighted !{% endem %}')
-      .withBookJson({"plugins": ["emphasize"]})
-      .create()
-      .then(function(result) {
-        expect(result.length).toEqual(1);
-        expect(result[0].content).toEqual('<p>This text is <span class="pg-emphasize pg-emphasize-yellow" style="">highlighted !</span></p>');
-      })
-      .fin(testDone)
-      .done();
-  });
+//  it('should create book with plugins and parse content', function(testDone) {
+//      tester.builder()
+//      .withContent('This text is {% em %}highlighted !{% endem %}')
+//      .withBookJson({"plugins": ["emphasize"]})
+//      .create()
+//      .then(function(result) {
+//        expect(result.length).toEqual(1);
+//        expect(result[0].content).toEqual('<p>This text is <span class="pg-emphasize pg-emphasize-yellow" style="">highlighted !</span></p>');
+//      })
+//      .fin(testDone)
+//      .done();
+//  });
 });
 
